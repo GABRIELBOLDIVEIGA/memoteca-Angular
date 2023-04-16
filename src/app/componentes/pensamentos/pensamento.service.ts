75e8347c144ec117fa2101f1d116aeb134940f5a
Gabriel Boldi <gabriel.boldi@protonmail.com>
2023-04-16 13:22:31 -0300

    ...

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import { Pensamento } from './pensamento';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PensamentoService {
 
-  constructor(http: HttpClient) { }
+  private readonly API = "http://localhost:4500/pensamentos"
 
+  constructor(private http: HttpClient) { }
+
+  listar() {
+    return this.http.get<Pensamento[]>(this.API);
+  }
 }
